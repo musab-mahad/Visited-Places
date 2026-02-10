e0a38f1f81b477a7c900e8e4129ffc18fca7dfab
waamuscab <muscab9494@gmail.com>
2026-02-10 19:38:33 +0300

    places logic added

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log(`Linked script.js`);
+// Places LOGIC
+function Place(location, landmarks, season, notes) {
+  this.location = location;
+  this.landmarks = landmarks;
+  this.season = season;
+  this.notes = notes;
+}
