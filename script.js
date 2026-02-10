f8e565d52008b61df0286ef70ba6c198ecb57ea6
waamuscab <muscab9494@gmail.com>
2026-02-10 19:51:47 +0300

    data places

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ function Place(location, landmarks, season, notes) {
   this.season = season;
   this.notes = notes;
 }
+
+// DATA
+const places = [
+  new Place("Mogadishu", ["Lido Beach"], "All year", "Nice"),
+  new Place("Nairobi", ["Uhuru Park"], "All year", "Cool weather"),
+  new Place("Mombasa", ["Diani Beach"], "Dry season", "Very humid"),
+];
